223659de6c252d089879b9c051d523ebdc274918
hellinay <f.helinay@gmail.com>
2022-04-04 15:59:48 +0300

    initial redux

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { act } from "@testing-library/react";
+import { createStore } from "redux";
+
+
+function counterReducer(state={counter:0},action) {
+    
+    if(action.type==='increment'){
+        return {
+            counter:state.counter+1
+        }
+    }
+    else if (action.type==='decrement'){
+        return {
+            counter:state.counter-1
+        }
+    }
+
+    return state;
+
+}
+
+const store= createStore(counterReducer)
+
+export default store;
